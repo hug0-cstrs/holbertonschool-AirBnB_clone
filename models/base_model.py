fb6bce0dfe6269cf24e738bc9464a539ab475895
hug0-cstrs <hugo.casteras@outlook.fr>
2023-07-05 12:25:39 +0200

    change kwargs's loop

--- models/base_model.py
+++ models/base_model.py
@@ -19,20 +19,21 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
         else:
-            for key, val in kwargs.items():
+            for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    dataTime = "%Y-%m-%dT %H:%M:%S.%f"
-                    val = datetime.strptime(kwargs[key], dataTime)
+                    if type(value) is str:
+                        """checks for all parsed datetime variables"""
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
-                    setattr(self, key, val)
+                    setattr(self, key, value)
 
     def __str__(self):
         """
         method for named
         """
         nameClass = self.__class__.__name__
-        return ("[{}] ({}) {}".format(type(self).__name__,
-                                      self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(nameClass, self.id, self.__dict__))
 
     def save(self):
         """updates last update time
@@ -41,10 +42,31 @@ class BaseModel:
 
     def to_dict(self):
         """ Returns dict with all keys/values of __dict__ of the instance"""
-        copy_dict = self.__dict__.copy()
-        copy_dict['__class__'] = type(self).__name__
-        if not isinstance(copy_dict['created_at'], str):
-            copy_dict['created_at'] = self.created_at.isoformat()
-        if not isinstance(copy_dict['updated_at'], str):
-            copy_dict['updated_at'] = self.updated_at.isoformat()
-        return copy_dict
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
