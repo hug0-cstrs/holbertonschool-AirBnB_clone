838a93579d67a2e533a7c1975611b8d645d8fb46
hug0-cstrs <hugo.casteras@outlook.fr>
2023-07-05 14:47:47 +0200

    delete main test

--- models/base_model.py
+++ models/base_model.py
@@ -50,17 +50,3 @@ class BaseModel:
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         my_dict["__class__"] = self.__class__.__name__
         return my_dict
-
-
-all_objs = models.storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
