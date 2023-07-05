61f29628683fb93bd0f7d9e34d0eb0d2b1c845f2
hug0-cstrs <hugo.casteras@outlook.fr>
2023-07-05 14:46:18 +0200

    update base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ Parent class that will inherit
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -18,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new()
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -39,6 +41,7 @@ class BaseModel:
         """updates last update time
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Returns dict with all keys/values of __dict__ of the instance"""
@@ -47,3 +50,17 @@ class BaseModel:
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         my_dict["__class__"] = self.__class__.__name__
         return my_dict
+
+
+all_objs = models.storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
