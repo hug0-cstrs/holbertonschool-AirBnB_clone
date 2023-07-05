134449952a9057b71ba82784b707770f1ef4be8c
hug0-cstrs <hugo.casteras@outlook.fr>
2023-07-05 09:34:29 +0200

    Create base_models file that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,65 @@
+#!/usr/bin/pyhon3
+"""
+Parent class that will inherit
+"""
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """Defines all common attributes/methods
+    """
+    def __init__(self, *args, **kwargs):
+        """Initializes all attributes
+        """
+        if not kwargs:  # Si aucun argument nommé n'a été passé
+            self.id = str(uuid.uuid4())  # Génère un UUID unique et l'assigne à l'attribut 'id'
+            self.created_at = datetime.now()  # Permet de crée un nouvel objet datetime représentant la date et l'heure actuelles, et l'assigne à l'attribut created_at
+            self.updated_at = self.created_at  # donne la même valeur que created_at à l'attribut updated_at
+            storage.new(self)  # Stocke l'objet dans le système de stockage
+        else:
+            for key, val in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    dataTime = "%Y-%m-%dT %H:%M:%S.%f"
+                    val = datetime.strptime(kwargs[key], dataTime)
+                    # Convertit la valeur de 'created_at' ou 'updated_at' en objet datetime en utilisant le format spécifié
+
+                if key != '__class__':
+                    setattr(self, key, val)
+                    # ChatGPT m'a bcp aider si ce putain de if de merde là !!!
+                    # permet d'assigner la valeur de l'argument nommé à l'attribut correspondant de l'objet
+
+    def __str__(self):
+        """
+        method for named
+        """
+        nameClass = self.__class__.__name__
+        return ("[{}] ({}) {}".format(nameClass, self.id, self.__dict__))
+
+    def save(self):
+        """updates last update time
+        """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ Returns dict with all keys/values of __dict__ of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
