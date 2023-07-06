86113f335431c0010f52a4f6931eef1ed949b222
hug0-cstrs <hugo.casteras@outlook.fr>
2023-07-06 21:14:04 +0200

    change import module

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """init package"""
 
-from models.engine import file_storage
+from models.engine.file_storage import FileStorage
 
-storage = file_storage.FileStorage()
+
+storage = FileStorage()
 storage.reload()
