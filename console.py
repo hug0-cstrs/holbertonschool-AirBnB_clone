e67326655fb1ee12f807535a05ee1472a32d1500
hug0-cstrs <hugo.casteras@outlook.fr>
2023-07-07 10:39:40 +0200

    update file

--- console.py
+++ console.py
@@ -26,86 +26,65 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """creates a new instance"""
-        if len(args) == 0:
+        """Creates a new instance of BaseModel"""
+        if not args:
             print("** class name missing **")
-            return
-        token = args.split()
-
-        try:
-            newInstance = eval(token[0])()
-            newInstance.save()
-            print(newInstance.id)
-        except Exception:
-            print("** class doesn't exist **")
+        else:
+            class_name = args.split()[0]
+            if class_name not in ["BaseModel"]:
+                print("** class doesn't exist **")
+            else:
+                new_instance = BaseModel()
+                new_instance.save()
+                print(new_instance.id)
 
     def do_show(self, args):
         """Prints the string representation of an instance"""
-        token = args.split()
-
-        if len(token) == 0:
+        args_list = args.split()
+        if not args_list:
             print("** class name missing **")
-            return
-        if len(token) == 1:
-            print("** instance id missing **")
-            return
-        try:
-            eval(token[0])
-        except Exception:
+        elif args_list[0] not in ["BaseModel"]:
             print("** class doesn't exist **")
-
-        objDict = storage.all()
-        keyId = token[0] + "." + token[1]
-
-        try:
-            value = objDict[keyId]
-            print(value)
-        except KeyError:
-            print("** no instance found **")
+        elif len(args_list) < 2:
+            print("** instance id missing **")
+        else:
+            obj_key = args_list[0] + "." + args_list[1]
+            obj_dict = storage.all()
+            if obj_key in obj_dict:
+                print(obj_dict[obj_key])
+            else:
+                print("** no instance found **")
 
     def do_destroy(self, args):
-        """Deletes an instance based on the class name"""
-        token = args.split()
-
-        if len(args) == 0:
+        """Deletes an instance based on class name and id"""
+        args_list = args.split()
+        if not args_list:
             print("** class name missing **")
-            return
-        if token[1] == 0:
+        elif args_list[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(args_list) < 2:
             print("** instance id missing **")
-
-        try:
-            eval(token[0])
-        except Exception:
-            print("** class doesn't exist **")
-        objDict = storage.all()
-        keyId = token[0] + "." + token[1]
-
-        try:
-            del objDict[keyId]
-        except Exception:
-            print("** no instance found **")
-        storage.save()
-
-    def do_all(self, arg):
-        """ Prints string represention of all instances of a given class """
-
-        if not arg:
-            print("** class name missing **")
-            return
-
-        token = arg.split()
-
-        if token[0] not in theClasses:
-            print("** class doesn't exist **")
         else:
-            all_objs = storage.all()
-            newList = []
+            obj_key = args_list[0] + "." + args_list[1]
+            obj_dict = storage.all()
+            if obj_key in obj_dict:
+                del obj_dict[obj_key]
+                storage.save()
+            else:
+                print("** no instance found **")
 
-            for key, val in all_objs.items():
-                ob_name = val.__class__.__name__
-                if ob_name == token[0]:
-                    newList += [val.__str__()]
-            print(newList)
+    def do_all(self, args):
+        """Prints all instances or instances of a specific class"""
+        obj_dict = storage.all()
+        if not args:
+            print([str(obj) for obj in obj_dict.values()])
+        else:
+            class_name = args.split()[0]
+            if class_name not in ["BaseModel"]:
+                print("** class doesn't exist **")
+            else:
+                print([str(obj) for obj in obj_dict.values()
+                       if isinstance(obj, BaseModel)])
 
     def do_update(self, args):
         """ Updates an instance based on the class name and id """
